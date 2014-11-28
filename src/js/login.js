a682a3b81e44476e599eb44dc300e5c2c362d91e
Alex Scown <alex.scown@hp.com>
2014-11-28 17:00:42 +0000

    Document login page.

--- src/js/login.js
+++ src/js/login.js
@@ -1,3 +1,6 @@
+/**
+ * @module login-page/js/login
+ */
 define([
     'backbone',
     'js-whatever/js/location',
@@ -22,15 +25,93 @@ define([
         })
     };
 
-    return Backbone.View.extend({
+    /**
+     * @typedef LoginStrings
+     * @desc Strings used by the default template. Different templates may require different strings
+     * @property {object} error Object containing error strings. Any value taken by the error parameter should be a key
+     * in this object, with the value being the message to display, which will be passed to the template
+     * @property {string} defaultLogin Informs the user that they need to login with default credentials
+     * @property {string} important Prefix for newCreds and defaultLogin
+     * @property {string} infoSearchConfig First line of additional info
+     * @property {string} infoDefaultLogin Second line of additional info
+     * @property {string} infoPasswordCopyPaste Third line of additional info
+     * @property {string} less Label for button which hides additional info
+     * @property {string} login Label for the login button
+     * @property {string} more Label for button which shows additional info
+     * @property {string} newCreds Informs the user that they should login with new credentials
+     * @property {string} password Label for the password field
+     * @property {string} title Title for the form
+     * @property {string} username Label for the username field
+     */
+    /**
+     * @typedef LoginOptions
+     * @property {string} configUrl Url that users visit to initially configure the system
+     * @property {LoginStrings} strings Strings passed to the template
+     * @property {string} url Form submission url passed to the template
+     */
+    /**
+     * @name module:login-page/js/login.Login
+     * @desc Creates a new LoginPage. Calls {@link module:login-page/js/login.Login#render render}
+     * @constructor
+     * @param {LoginOptions} options
+     */
+    return Backbone.View.extend(/** @lends module:login-page/js/login.Login.prototype */{
 
+        /**
+         * @desc Backbone el property
+         * @default body
+         */
         el: 'body',
 
+        /**
+         * @typedef TemplateOptions
+         * @desc Options that are passed to the template
+         * @property {string} [defaultUsername] Username to pre-populate the username field with. Set to the value of
+         * the defaultLogin query parameter
+         * @property {string} [error] Error message to display. Set to strings.error[&lt;value of error query parameter&gt;]
+         * @property {string} [expandTemplate] Initial state of the more info button, which in the default template will
+         * be rendered if isDefaultLogin is true
+         * @property {boolean} [isDefaultLogin=false] If true, displays additional instructions about the default login
+         * and makes the username input readonly. Should be used in conjunction with defaultUser. Set to true if the
+         * defaultLogin query parameter exists
+         * @property {boolean} [isNewLogin=false] Displays a message if set to true. Takes priority over isDefaultLogin.
+         * Set to true if document.referrer contains {@link LoginOptions#configUrl}
+         * @property {boolean} [isZkConfig=false] Displays a message if set to true. Set to true if the isZkConfig query
+         * parameter is equal to true. Not used by the default template
+         * @property {LoginStrings} strings Strings passed to the template
+         * @property {string} url Url that the form will be posted to
+         * @property {string} [username] Username to pre-populate the username field with if defaultUsername is not
+         * defined. Set to the username query parameter
+         */
+        /**
+         * @desc Template function.
+         * @function
+         * @param {TemplateOptions} options Options passed to the template at render time
+         */
         template: _.template(template),
 
+        /**
+         * @desc CSS class used for the show less button when in default login mode
+         * @default icon-minus
+         */
         iconMinusClass: 'icon-minus',
+
+        /**
+         * @desc CSS class used for the show more button when in default login mode
+         * @default icon-plus
+         */
         iconPlusClass: 'icon-plus',
+
+        /**
+         * @desc CSS class used for grouping controls. Set this to form-group if using Bootstrap 3
+         * @default control-group
+         */
         controlGroupClass: 'control-group',
+
+        /**
+         * @desc CSS class used to indicate errors. Set to has-error if using Bootstrap 3
+         * @default error
+         */
         errorClass: 'error',
 
         initialize: function(options) {
@@ -41,6 +122,9 @@ define([
             this.render();
         },
 
+        /**
+         * @desc Renders the login view. You should not need to call this manually.
+         */
         render: function() {
             var usernameParam = /[&?]username=([^&]+)/.exec(location.search());
             var errorParam = /[&?]error=([^&]+)/.exec(location.search());
@@ -98,6 +182,11 @@ define([
             }, this));
         },
 
+        /**
+         * @desc Called when the login button is clicked. Submits the form if all of the inputs are not empty, or adds
+         * {@link module:login-page/js/login.Login#errorClass errorClass} to empty inputs
+         * @param {Event} e jQuery event object
+         */
         login: function(e) {
             e.preventDefault();
 
